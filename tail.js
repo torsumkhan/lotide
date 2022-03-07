9053642a246679efd34a66072e739d05880a9204
torsumkhan <torsum.khan@gmail.com>
2022-03-08 04:16:13 +0500

    created tail function

---
+++ tail.js
@@ -0,0 +1,19 @@
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙌 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Asserion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
