ee4eec9d481f87f0ec6ec460f651f39fcb004cef
torsumkhan <torsum.khan@gmail.com>
2022-03-15 01:43:22 +0500

    create index file for module

---
+++ index.js
@@ -0,0 +1,29 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const without = require('./without');
+
+
+module.exports = {
+    head: head,
+    tail: tail,
+    middle: middle,
+    assertArraysEqual: assertArraysEqual,
+    assertEqual: assertEqual,
+    countLetters: countLetters,
+    countOnly: countOnly,
+    findKey: findKey,
+    findKeyByValue: findKeyByValue,
+    without: without,
+    map: map,
+    letterPositions: letterPositions
+
+}
