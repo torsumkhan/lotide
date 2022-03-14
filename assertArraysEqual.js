18f857d6e55d56af048e16131d0c7cf94a8a3d47
torsumkhan <torsum.khan@gmail.com>
2022-03-14 23:24:48 +0500

    assertarraysequal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,11 @@
-function eqArrays(arr1,arr2) {
-    if(arr1.length !== arr2.length) {
-        return false;
-    } else {
-        for (let i = 0; i < arr1.length; i ++) {
-          if(arr1[i] !== arr2[i]){
-              return false
-          }
-        }
-      return true;
-    }
-}
+const eqArrays = require('./eqArrays');
 
-function assertArraysEqual(arr1,arr2){
-    if (eqArrays(arr1,arr2) === true) {
-        console.log(`🙌 Assertion Passed: ${arr1} === ${arr2}`);
-      } else {
-        console.log(`❌ Asserion Failed: ${arr1} !== ${arr2}`);
-      }
-}
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+module.exports = assertArraysEqual;
