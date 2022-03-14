f87111daa91d02aa92782127d1dd06d209f04766
torsumkhan <torsum.khan@gmail.com>
2022-03-15 00:59:26 +0500

    mocha and chai for tailtest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,13 @@
-const assertEqual = require('../assertEqual')
-const tail = require('../tail')
+const assert = require('chai').assert;
+const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+
+
+describe("tail", () => {
+    it("returns [1,2] for [1, 2, 3]", () => {
+      assert.deepEqual(tail([1, 2, 3]), [2,3]);
+    });
+  });
